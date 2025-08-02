f6770821608ee864324efc685fe3bb58e12499fe
Piyush Kaira <piyushkaira666@gmail.com>
2025-08-02 15:35:31 +0530

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    is_staff = models.BooleanField(default=False)  # Used to distinguish staff vs normal user
+    is_staff = models.BooleanField(default=False)  
+
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
